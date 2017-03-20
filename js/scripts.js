b80682363158ffb9b1a36fcde402c15e716fafa1
Micky Matthews <mixiemoxie@gmail.com>
2017-03-20 11:14:03 -0700

    Add business logic to scripts.js including for loop.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Business logic
+var myFunction = function(userNumber) {
+  var results = []; // The function returns an array
+
+  for (var i = 1 ; i <= userNumber; i ++) {
+  // sets the variable before the loop starts (var i = 1), defines the condition for the loop to run, then increases the value each time the code block in the loop has been executed
+    if (i % 2 === 0) {
+      outcome.push(userNumber*2);
+    } else if (i % 3 === 0) {
+      outcome.push(userNumber*3);
+    } else {
+      outcome.push(i);
+    }
+  }
+  return outcome;
+};
+
+// User interface logic. Gets the user input and runs that number through the business logic and displays each item in the array as a list item.

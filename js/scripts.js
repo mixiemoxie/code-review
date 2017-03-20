ebbf12c0fc45a984154e4c40e5a930f4ab35d4a7
Micky Matthews <mixiemoxie@gmail.com>
2017-03-20 15:13:20 -0700

    Correct business logic to return var results vs. outcome.

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,14 @@ var myFunction = function(userNumber) {
   for (var i = 1 ; i <= userNumber; i ++) {
   // sets the variable before the loop starts (var i = 1), defines the condition for the loop to run, then increases the value each time the code block in the loop has been executed
     if (i % 2 === 0) {
-      outcome.push(userNumber*2);
+      results.push(userNumber*2);
     } else if (i % 3 === 0) {
-      outcome.push(userNumber*3);
+      results.push(userNumber*3);
     } else {
-      outcome.push(i);
+      results.push(i);
     }
   }
-  return outcome;
+  return results;
 };
 
 // User interface logic. Gets the user input and runs that number through the business logic and displays each item in the array as a list item.

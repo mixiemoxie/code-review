8d971b52cb14a725a008a99dec6789b7037673bd
Micky Matthews <mixiemoxie@gmail.com>
2017-03-20 15:55:58 -0700

    Fix event listener from form submit to button click.

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,9 @@ var myFunction = function(userNumber) {
   for (var i = 1 ; i <= userNumber; i ++) {
   // sets the variable before the loop starts (var i = 1), defines the condition for the loop to run, then increases the value each time the code block in the loop has been executed
     if (i % 2 === 0) {
-      results.push(userNumber*2);
+      results.push(userNumber * 2);
     } else if (i % 3 === 0) {
-      results.push(userNumber*3);
+      results.push(userNumber * 3);
     } else {
       results.push(i);
     }
@@ -17,10 +17,17 @@ var myFunction = function(userNumber) {
 
 // User interface logic. Gets the user input and runs that number through the business logic and displays each item in the array as a list item.
 $(document).ready(function() {
-  $("form#form").submit(function(event) {
-    event.preventDefault();
+  $("button#submit").click(function(event) {
+    $("#outcome").empty();
     var userNumber = parseInt($("input#number").val());
     var outcome = myFunction(userNumber);
 
+    $("#outcome").append("<ul>");
+      outcome.forEach(function(singleResult)  {
+    $("#outcome").append("<li>" + singleResult + "</li");
+    });
+    $("#outcome").append("</ul");
+
+      event.preventDefault();
   });
 });
